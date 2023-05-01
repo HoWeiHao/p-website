c57f07baea3351896f111c9086647063a3322518
HoWeiHao <howeihao3105@gmail.com>
2023-05-01 20:07:12 +0800

    Changed the texts to reflect my own name.

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,9 +8,9 @@ export default async function Home() {
   return (
     <div>
       <h1 className="text-7xl font-extrabold">Hello I&apos;m
-        <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent"> Kapehe!</span>
+        <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent"> Wei Hao!</span>
       </h1>
-      <p className="mt-3 text-xl text-gray-600">Aloha everyone! Check out my projects!</p>
+      <p className="mt-3 text-xl text-gray-600">Check out my projects so far!</p>
       <h2 className="mt-24 font-bold text-gray-700 text-3xl">My Projects</h2>
 
       <div className="mt-5 grid md:grid-cols-2 lg:grid-cols-3 gap-8">{projects.map((project) => (
