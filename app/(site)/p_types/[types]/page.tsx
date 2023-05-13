b658b3c3773bd93c946fd6a984ae5aa19856b668
HoWeiHao <howeihao3105@gmail.com>
2023-05-14 02:28:53 +0800

    Edited the projects page

--- app/(site)/p_types/[types]/page.tsx
+++ app/(site)/p_types/[types]/page.tsx
@@ -9,23 +9,24 @@ type Props = {
 export default async function Projects({ params }: Props) {
   const projects = await getProjects(params.types);
   
+  const formattedString = params.types.charAt(0).toUpperCase() + params.types.slice(1).replace(/-/g, ' ');
 
   return (
     <div className="items-center justify-between ">
-      <h1 className="text-blue-700 text-5xl drop-shadow font-extrabold py-3"></h1>  
+      <h1 className="text-blue-700 text-5xl drop-shadow font-extrabold py-3 mx-40">{ formattedString }</h1>  
 
-      <div className="mt-5 grid md:grid-cols-2 lg:grid-cols-3 gap-8">{projects.map((project) => (
+      <div className="mt-5 grid md:grid-cols-2 lg:grid-cols-3 gap-8 mx-40">{projects.map((project) => (
         <Link key={project._id} href={`${project.slug}`}  className="border-2 border-gray-500 rounded-lg p-1 hover:scale-105 hover:border-blue-500 transition">
           {project.image && (
             <Image
               src={project.image}
               alt={project.name}
-              width={750}
-              height={300}
-              className="object-cover rounded-lg border border-gray-500"
+              width={300}
+              height={50}
+              className="object-cover"
             />
           )}
-          <div className="py-5 mt-2 font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
+          <div className="mt-2 font-extrabold text-gray-800">
             {project.name}
           </div>
         </Link>
