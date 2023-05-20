d8604bfbfeba0b14779ad5f2f5d9e7c13b75ac4b
HoWeiHao <howeihao3105@gmail.com>
2023-05-20 21:39:05 +0800

    Added new project type "Games"

--- types/p_type-list.ts
+++ types/p_type-list.ts
@@ -1,3 +1,3 @@
-const p_types =   ["Animations", "3D-Modelling", "Engineering Projects"] 
+const p_types =   ["Animations", "3D-Modelling", "Engineering Projects", "Games"] 
 
 export default p_types
